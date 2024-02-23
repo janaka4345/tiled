ba69a703a2f001dac917420b55d689096844c222
banuka <banukaramanayaka76@gmail.com>
2024-02-23 20:25:37 +0530

    other:

--- app/page.js
+++ app/page.js
@@ -1,10 +1,49 @@
+"use client";
+import Pattern2 from "@/components/Pattern2";
+import { useRef } from "react";
+import * as htmlToImage from "html-to-image";
 
-import Link from "next/link"
+export default function page() {
+  const svgRef = useRef();
 
-export default function Home() {
+  function handleClick() {
+    const svgNode = svgRef.current;
+    let blob = "";
+
+    // Convert the SVG node to an image
+    htmlToImage
+      .toPng(svgNode)
+      .then(function (dataUrl) {
+        // Once converted, you can handle the dataUrl (image URL)
+        blob = new Blob([dataUrl], { type: "image/png" });
+        console.log(blob);
+        // console.log(dataUrl);
+        // You can also create an <img> element and append it wherever you want, or use it in any other way
+      })
+      .catch(function (error) {
+        console.error("Error:", error);
+      });
+  }
+
   return (
-    <div>
-      <Link href='/test'>test</Link>
-    </div>
-  )
+    <>
+      <div
+        ref={svgRef}
+        className="grid grid-rows-3 grid-cols-3 w-[600px] h-[600px]"
+      >
+        {[...Array(9)].map((_, i) => (
+          <Pattern2 key={i} />
+        ))}
+      </div>
+      <button className="bg-green-500 p-4 rounded-lg" onClick={handleClick}>
+        Save
+      </button>
+      <button
+        className="bg-red-500 p-4 rounded-lg"
+        onClick={() => console.log("clear")}
+      >
+        Clear All
+      </button>
+    </>
+  );
 }
