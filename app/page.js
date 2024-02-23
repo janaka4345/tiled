a0e7ee349c3a2f9b20d00a74978d4444d5f09840
banuka <banukaramanayaka76@gmail.com>
2024-02-23 20:23:15 +0530

    other:

--- app/page.js
+++ app/page.js
@@ -1,14 +1,9 @@
-'use client'
-import Pattern from "@/components/Pattern"
+
 import Link from "next/link"
-import { useState } from "react"
 
 export default function Home() {
-  const [color, setColor] = useState(false)
   return (
     <div>
-
-      <Pattern />
       <Link href='/test'>test</Link>
     </div>
   )
