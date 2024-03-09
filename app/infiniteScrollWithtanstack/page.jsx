d122575a66b82b46882770966df93b74fd4a1af0
banuka <banukaramanayaka76@gmail.com>
2024-03-09 21:58:55 +0530

    fix:loading empty arrays

--- app/infiniteScrollWithtanstack/page.jsx
+++ app/infiniteScrollWithtanstack/page.jsx
@@ -5,7 +5,7 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 //mock a data  fetch
 async function fetchPosts(page) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("resolved");
+
   return posts.slice((page - 1) * 2, page * 2);
 }
 
@@ -14,13 +14,18 @@ export default function page() {
   const data = useInfiniteQuery({
     queryKey: ["query"],
     queryFn: ({ pageParam }) => {
-      console.log({ pageParam });
+      // console.log({ pageParam });
       const response = fetchPosts(pageParam);
       return response;
     },
     initialPageParam: 1,
-    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) =>
-      lastPageParam + 1,
+    getNextPageParam: (lastPage, allPages, lastPageParam, allPageParams) => {
+      // console.log({ lastPage, allPages, lastPageParam, allPageParams });
+      if (lastPage.length === 0) {
+        return null;
+      }
+      return lastPageParam + 1;
+    },
     getPreviousPageParam: (
       firstPage,
       allPages,
@@ -31,8 +36,10 @@ export default function page() {
 
   return (
     <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <button onClick={data.fetchNextPage}>click</button>
+      <pre>{JSON.stringify(data.data, null, 2)}</pre>
+      <button disabled={!data.hasNextPage} onClick={data.fetchNextPage}>
+        click
+      </button>
       {/* <pre>{JSON.stringify(data.fetchNextPage, null, 2)}</pre> */}
     </div>
   );
