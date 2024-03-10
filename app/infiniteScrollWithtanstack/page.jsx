9dc7adde24da4248b67f1049556a4f4a5a3930f6
banuka <banukaramanayaka76@gmail.com>
2024-03-10 07:43:56 +0530

    feat:infinite scroll with useInView

--- app/infiniteScrollWithtanstack/page.jsx
+++ app/infiniteScrollWithtanstack/page.jsx
@@ -1,6 +1,8 @@
 "use client";
 import posts from "@/data";
 import { useInfiniteQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
 
 //mock a data  fetch
 async function fetchPosts(page) {
@@ -11,6 +13,8 @@ async function fetchPosts(page) {
 
 export default function page() {
   // { data, fetchNextPage, isFetchingNextPage }
+  const { ref, inView, entry } = useInView();
+
   const data = useInfiniteQuery({
     queryKey: ["query"],
     queryFn: ({ pageParam }) => {
@@ -34,10 +38,19 @@ export default function page() {
     ) => firstPageParam - 1,
   });
 
+  useEffect(() => {
+    data.hasNextPage && inView ? data.fetchNextPage() : null;
+    // console.log({ ref, inView, entry });
+  }, [inView, data]);
+
   return (
     <div>
-      <pre>{JSON.stringify(data.data, null, 2)}</pre>
-      <button disabled={!data.hasNextPage} onClick={data.fetchNextPage}>
+      <pre>{JSON.stringify(data?.data?.pages, null, 2)}</pre>
+      <button
+        ref={ref}
+        disabled={!data.hasNextPage}
+        onClick={data.fetchNextPage}
+      >
         click
       </button>
       {/* <pre>{JSON.stringify(data.fetchNextPage, null, 2)}</pre> */}
---
+++ app/serverCheck/page.jsx
@@ -0,0 +1,9 @@
+export default function page() {
+  console.log("code");
+  return (
+    <div>
+      <h1>page</h1>
+      {console.log("jsx")}
+    </div>
+  );
+}
