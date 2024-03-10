75d8af538a00a13ad376860e7805a59c39450546
banuka <banukaramanayaka76@gmail.com>
2024-03-10 13:48:16 +0530

    oher:

--- server/routes/placeholder.js
+++ server/routes/placeholder.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import { publicProcedure, router } from '../trpc';
-import { z } from "zod";
-const placeholder = router({
-    infiniteProductList: publicProcedure
-        .input(z.object({
-            limit: z.number().min(1).max(100).default(1),
-            cursor: z.number().nullish(),
+// import axios from "axios";
+// import { publicProcedure, router } from '../trpc';
+// import { z } from "zod";
+// const placeholder = router({
+//     infiniteProductList: publicProcedure
+//         .input(z.object({
+//             limit: z.number().min(1).max(100).default(1),
+//             cursor: z.number().nullish(),
 
-        }))
-        .query(async ({ input }) => {
-            const { limit, cursor } = input
-            // Retrieve users from a datasource, this is an imaginary database
-            const response = await axios.get('https://dummyjson.com/products'
-                , {
-                    params: {
-                        limit,
-                        cursor
-                    }
-                }
-            )
-            console.log(response.data);
-            const products = response?.data?.products
-            return products
-        }),
-});
-export { placeholder }
+//         }))
+//         .query(async ({ input }) => {
+//             const { limit, cursor } = input
+//             // Retrieve users from a datasource, this is an imaginary database
+//             const response = await axios.get('https://dummyjson.com/products'
+//                 , {
+//                     params: {
+//                         limit,
+//                         cursor
+//                     }
+//                 }
+//             )
+//             console.log(response.data);
+//             const products = response?.data?.products
+//             return products
+//         }),
+// });
+// export { placeholder }
